428936bc8b03858c3452be3135d1c6e06506351a
Aydin Ghane Kh. <aydin.ghane.kh@gmail.com>
2025-12-10 18:12:21 +0100

    Render shipments in table

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,37 +1,56 @@
-import { useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { useAppSelector } from "../../app/hooks";
 import { useGetDataQuery } from "../../features/data/data-api";
 import { Dialog } from "../../features/dialog/components/Dialog";
-import {
-  openDialog,
-  selectDialogOpened,
-} from "../../features/dialog/dialog-slice";
+import { selectDialogOpened } from "../../features/dialog/dialog-slice";
 import "./App.scss";
 
 export const App: React.FC = () => {
-  const dispatch = useAppDispatch();
-  const { data, isLoading, isError } = useGetDataQuery();
+  const { data: shipments, isLoading, isError } = useGetDataQuery();
   const isOpened = useAppSelector(selectDialogOpened);
 
-  useEffect(() => {
-    if (data) {
-      console.log("Fetched data:", data);
-    }
-  }, [data]);
-
   if (isLoading) {
-    return <div className="app">Loading...</div>;
+    return <div className="centered">Loading...</div>;
   }
 
   if (isError) {
-    return <div className="app">Error loading data.</div>;
+    return <div className="centered">Error loading data.</div>;
+  }
+
+  if (!shipments) {
+    return <div className="centered">No data available.</div>;
   }
 
   return (
     <div className="app">
-      <button type="button" onClick={() => dispatch(openDialog())}>
-        Open dialog
-      </button>
+      <header>Shipment Dashboard</header>
+
+      <main>
+        <table>
+          <caption>Total Shipments: {shipments.length}</caption>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Origin</th>
+              <th>Status</th>
+              <th>Destination</th>
+              <th>ETA</th>
+            </tr>
+          </thead>
+          <tbody>
+            {shipments.map((shipment) => (
+              <tr key={shipment.id}>
+                <td>{shipment.id}</td>
+                <td>{shipment.origin}</td>
+                <td>{shipment.status}</td>
+                <td>{shipment.destination}</td>
+                <td>{new Date(shipment.estimatedArrival).toLocaleString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+
+      <footer>Tech assignment</footer>
 
       {isOpened && <Dialog />}
     </div>
